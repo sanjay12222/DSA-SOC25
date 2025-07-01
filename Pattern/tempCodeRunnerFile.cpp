0c74529c87962a46ee1d240cf0cbab6665ea3a5c
sanjay meena <22b3978@iitb.ac.in>
2025-07-01 19:31:06 +0530

    Arrays Problems

---
+++ Pattern/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+    int i, j;
+    for (i = 1; i < 5; i++)
+    {
+        for (j = 1; j <= 5 - i; j++)
+        {
+            cout << "*-";
+        }
+        cout << endl;
+    }
+}
